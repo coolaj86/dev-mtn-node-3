b368e5d8f7d06a36ac1f5b933412a5bbb0d01b07
AJ ONeal <awesome@coolaj86.com>
2015-06-22 10:38:15 -0600

    add static file serving

--- app.js
+++ app.js
@@ -4,5 +4,11 @@ module.exports = function create() {
   var express = require('express');
   var app = express();
 
+  var path = require('path');
+  var pubPath = path.join(__dirname, 'public');
+
+  // __dirname == the directory where this script file is
+  app.use('/', express.static(pubPath));
+
   return app;
 };
