d31a13cc50d68f6a33a7306370981f0fd3228862
AJ ONeal <awesome@coolaj86.com>
2015-06-22 11:44:18 -0600

    add body-parser

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ module.exports = function create() {
   var express = require('express');
   var app = express();
   var api = require('./api');
+  var bodyParser = require('body-parser');
 
   var path = require('path');
   var pubPath = path.join(__dirname, 'public');
 
   // __dirname == the directory where this script file is
+  app.use(bodyParser.json());
   app.use('/api', api());
   app.use('/', express.static(pubPath));
 
